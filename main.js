4f6539124d28bda7887f143899a70196a794c480
Mehmet Mert Yıldıran <mehmetmertyildiran@gmail.com>
2016-09-28 03:23:34 +0300

    Main window added.

--- main.js
+++ main.js
@@ -6,17 +6,37 @@ const BrowserWindow = electron.BrowserWindow
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
+let loadingWindow
 let mainWindow
 
-function createWindow () {
+function createLoadingWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600, transparent: true, frame: false})
+  loadingWindow = new BrowserWindow({width: 800, height: 600, transparent: true, frame: false})
+
+  // and load the loading.html of the app.
+  loadingWindow.loadURL(`file://${__dirname}/loading.html`)
+
+  // Open the DevTools.
+  //loadingWindow.webContents.openDevTools()
+
+  // Emitted when the window is closed.
+  loadingWindow.on('closed', function () {
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    loadingWindow = null
+  })
+}
+
+function createMainWindow () {
+  // Create the browser window.
+  mainWindow = new BrowserWindow({width: 1280, height: 768, transparent: true, frame: false})
 
   // and load the index.html of the app.
   mainWindow.loadURL(`file://${__dirname}/index.html`)
 
   // Open the DevTools.
-  //mainWindow.webContents.openDevTools()
+  //indexWindow.webContents.openDevTools()
 
   // Emitted when the window is closed.
   mainWindow.on('closed', function () {
@@ -30,7 +50,11 @@ function createWindow () {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
+app.on('ready', function() {
+    createLoadingWindow();
+    //createMainWindow();
+    setTimeout(createMainWindow, 3000);
+})
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function () {
@@ -44,8 +68,8 @@ app.on('window-all-closed', function () {
 app.on('activate', function () {
   // On OS X it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) {
-    createWindow()
+  if (loadingWindow === null) {
+    createMainWindow()
   }
 })
 
