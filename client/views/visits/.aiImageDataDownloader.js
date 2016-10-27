c86823a0b2be2764db57cf906b4358a74f0a00e0
Mehmet Mert Yıldıran <mehmetmertyildiran@gmail.com>
2016-10-27 10:46:08 +0300

    ImageData downloader | Looping through diagnoses dictionary added.

--- client/views/visits/.aiImageDataDownloader.js
+++ client/views/visits/.aiImageDataDownloader.js
@@ -2,43 +2,48 @@ var request = require('request');
 var fs = require('fs');
 var https = require('https');
 var im = require('imagemagick');
+require('./constants.js');
 
 IMAGE_SIZE = '64';
 
-var diagnosis_id = '109487';
-if (!fs.existsSync(diagnosis_id)){
-    fs.mkdirSync(diagnosis_id);
-}
+for (var key in DIAGNOSES_DICT_SWAP) {
 
-diagnosisString = 'diagnosis=' + diagnosis_id;
-var dermQuestUrl = 'https://www.dermquest.com/Services/imageData.ashx?' + diagnosisString + '&page=1&perPage=500&sort=relevance';
-var largeImageUrl = 'https://www.dermquest.com/imagelibrary/large/';
+    var diagnosis_id = key;
+    if (!fs.existsSync(diagnosis_id)){
+        fs.mkdirSync(diagnosis_id);
+    }
 
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0"; // Replace this line with rejectUnauthorized: false,
-request(dermQuestUrl, function (error, response, body) {
-  if (!error && response.statusCode == 200) {
-    var dJson = JSON.parse(body);
-    dJson['Results'].forEach(function(element, index, array) {
+    diagnosisString = 'diagnosis=' + diagnosis_id;
+    var dermQuestUrl = 'https://www.dermquest.com/Services/imageData.ashx?' + diagnosisString + '&page=1&perPage=500&sort=relevance';
+    var largeImageUrl = 'https://www.dermquest.com/imagelibrary/large/';
 
-        console.log('Request: ' + element.FileName);
+    process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0"; // Replace this line with rejectUnauthorized: false,
+    request(dermQuestUrl, function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+        var dJson = JSON.parse(body);
+        dJson['Results'].forEach(function(element, index, array) {
 
-        var file = fs.createWriteStream(diagnosis_id + '/' + element.FileName);
-        file.on('finish', function () {
-            //convert 010020VB.JPG -resize 64x64^ -gravity center -crop 64x64+0+0 result.jpg
-            im.convert([diagnosis_id + '/' + element.FileName, '-resize', IMAGE_SIZE + 'x' + IMAGE_SIZE + '^', '-gravity', 'center', '-crop', IMAGE_SIZE + 'x' + IMAGE_SIZE + '+0+0', diagnosis_id + '/' + element.FileName],
-                function(err, stdout){
-                  if (err) throw err;
-                  console.log('\t\tResize ' + IMAGE_SIZE + 'x' + IMAGE_SIZE + ': ' + element.FileName + ' ', stdout);
+            console.log('Request: ' + element.FileName);
+
+            var file = fs.createWriteStream(diagnosis_id + '/' + element.FileName);
+            file.on('finish', function () {
+                //convert 010020VB.JPG -resize 64x64^ -gravity center -crop 64x64+0+0 result.jpg
+                im.convert([diagnosis_id + '/' + element.FileName, '-resize', IMAGE_SIZE + 'x' + IMAGE_SIZE + '^', '-gravity', 'center', '-crop', IMAGE_SIZE + 'x' + IMAGE_SIZE + '+0+0', diagnosis_id + '/' + element.FileName],
+                    function(err, stdout){
+                      if (err) throw err;
+                      console.log('\t\tResize ' + IMAGE_SIZE + 'x' + IMAGE_SIZE + ': ' + element.FileName + ' ', stdout);
+                });
             });
-        });
 
-        https.get(largeImageUrl + element.FileName, function(response) {
-            if (!error && response.statusCode == 200) {
-                console.log('\tPipe: ' + element.FileName);
-                response.pipe(file);
-            }
-        });
+            https.get(largeImageUrl + element.FileName, function(response) {
+                if (!error && response.statusCode == 200) {
+                    console.log('\tPipe: ' + element.FileName);
+                    response.pipe(file);
+                }
+            });
 
+        });
+      }
     });
-  }
-});
+
+}
