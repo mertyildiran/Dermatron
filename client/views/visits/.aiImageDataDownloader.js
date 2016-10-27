3c9e4dc0be4f3ba757f5ecd13b65582dbef09e46
Mehmet Mert Yıldıran <mehmetmertyildiran@gmail.com>
2016-10-27 08:26:11 +0300

    ImageData downloader | Image resizing added.

--- client/views/visits/.aiImageDataDownloader.js
+++ client/views/visits/.aiImageDataDownloader.js
@@ -1,6 +1,9 @@
 var request = require('request');
 var fs = require('fs');
 var https = require('https');
+var im = require('imagemagick');
+
+IMAGE_SIZE = '64';
 
 var diagnosis_id = '109487';
 if (!fs.existsSync(diagnosis_id)){
@@ -17,16 +20,25 @@ request(dermQuestUrl, function (error, response, body) {
     var dJson = JSON.parse(body);
     dJson['Results'].forEach(function(element, index, array) {
 
-        console.log(element.FileName);
+        console.log('Request: ' + element.FileName);
 
         var file = fs.createWriteStream(diagnosis_id + '/' + element.FileName);
+        file.on('finish', function () {
+            //convert 010020VB.JPG -resize 64x64^ -gravity center -crop 64x64+0+0 result.jpg
+            im.convert([diagnosis_id + '/' + element.FileName, '-resize', IMAGE_SIZE + 'x' + IMAGE_SIZE + '^', '-gravity', 'center', '-crop', IMAGE_SIZE + 'x' + IMAGE_SIZE + '+0+0', diagnosis_id + '/' + element.FileName],
+                function(err, stdout){
+                  if (err) throw err;
+                  console.log('\t\tResize ' + IMAGE_SIZE + 'x' + IMAGE_SIZE + ': ' + element.FileName + ' ', stdout);
+            });
+        });
+
         https.get(largeImageUrl + element.FileName, function(response) {
             if (!error && response.statusCode == 200) {
-                console.log('piping');
+                console.log('\tPipe: ' + element.FileName);
                 response.pipe(file);
             }
         });
 
     });
   }
-})
+});
